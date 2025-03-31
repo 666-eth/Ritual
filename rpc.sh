5075e5fab717043cb8cadd651c8f63027b989c32
666-eth <77118877+666-eth@users.noreply.github.com>
2025-04-01 03:47:12 +0800

    Update rpc.sh

--- rpc.sh
+++ rpc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 RPC_URL="https://base.llamarpc.com"
-sudo sed -i 's|"rpc_url": ".*"|"rpc_url": "https://rpc.ankr.com/base/1f271c0661c150d62029d9b818ef3179cfb8be6c68c648d65d96b3d2b7028cbd"|' /root/infernet-container-starter/deploy/config.json
-sudo sed -i 's|"rpc_url": ".*"|"rpc_url": "https://rpc.ankr.com/base/1f271c0661c150d62029d9b818ef3179cfb8be6c68c648d65d96b3d2b7028cbd"|' /root/infernet-container-starter/projects/hello-world/container/config.json
+sudo sed -i 's|"rpc_url": ".*"|"rpc_url": "https://base.llamarpc.com"|' /root/infernet-container-starter/deploy/config.json
+sudo sed -i 's|"rpc_url": ".*"|"rpc_url": "https://base.llamarpc.com"|' /root/infernet-container-starter/projects/hello-world/container/config.json
 sudo sed -i "s|^RPC_URL := .*|RPC_URL := $RPC_URL|" /root/infernet-container-starter/projects/hello-world/contracts/Makefile
 sudo docker compose -f /root/infernet-container-starter/deploy/docker-compose.yaml down
 sudo docker compose -f /root/infernet-container-starter/deploy/docker-compose.yaml up -d
